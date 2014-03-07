0e3c48d07d13fb43cabf730ec89fe276c8a7a526
Noel Hwande <noel.hwande@gmail.com>
2014-03-07 16:24:44 -0600

    Completed Level 3.2

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var App = Ember.Application.create({
 
 App.Router.map(function() {
 	this.route('about');
+    this.resource('products');
 });
 
 App.IndexController = Ember.Controller.extend({
@@ -16,8 +17,38 @@ App.IndexController = Ember.Controller.extend({
 
 App.AboutController = Ember.Controller.extend({
     contactName: "Noel Hwande",
-    avatar: 'images/avatar.png',
+    avatar: 'http://placehold.it/24',
     open: function() {
         return ((new Date()).getDay() === 1) ? "Sorry, we are closed today." : "Please come in. We're open";
     }.property()
 });
+
+App.ProductsRoute = Ember.Route.extend({
+    model: function() {
+        return App.PRODUCTS;
+    }
+});
+
+App.PRODUCTS = [
+    {
+        title: 'Flint',
+        price: 99,
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat, dui vitae sodales laoreet, eros enim accumsan neque, sit amet eleifend dolor urna ultricies diam. Mauris ac pellentesque odio, ut adipiscing massa. Integer eget tempor sem. Mauris sodales mi ultricies, ornare velit vel, mollis nibh. ',
+        isOnSale: true,
+        image: 'http://placehold.it/250'
+    },
+    {
+        title: 'Kindling',
+        price: 249,
+        description: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed semper eros ipsum, in condimentum leo pulvinar id. Aliquam egestas ut mi non vestibulum. Phasellus laoreet at enim vitae porttitor. Mauris pulvinar auctor justo, nec pulvinar dui. Ut placerat pulvinar dolor, ac lobortis purus. Proin ornare eget odio ac facilisis.',
+        isOnSale: false,
+        image: 'http://placehold.it/250'
+    },
+    {
+        title: 'Matches',
+        price: 39,
+        description: 'Suspendisse tincidunt justo eu diam tincidunt auctor. Nam ac odio lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque varius euismod auctor. Ut sollicitudin dui eu magna vestibulum hendrerit. In hac habitasse platea dictumst. Praesent sit amet consectetur ligula. ',
+        isOnSale: true,
+        image: 'http://placehold.it/250'
+    }
+];

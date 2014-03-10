fab09595843161eb0fd4d704e74f5865298ce1bd
Noel Hwande <noel.hwande@gmail.com>
2014-03-10 11:22:41 -0500

    Completed Level 4.2

--- js/app.js
+++ js/app.js
@@ -5,9 +5,11 @@ var App = Ember.Application.create({
 App.Router.map(function() {
 	this.route('about');
     this.resource('products', function() {
-      this.resource('product', {path: '/:title'});
+      this.resource('product', {path: '/:product_id'});
     });
 });
+
+App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 App.IndexController = Ember.Controller.extend({
     productsCount: 75,
@@ -27,18 +29,27 @@ App.AboutController = Ember.Controller.extend({
 
 App.ProductsRoute = Ember.Route.extend({
     model: function() {
-        return App.PRODUCTS;
+        return this.store.findAll('product');
     }
 });
 
 App.ProductRoute = Ember.Route.extend({
     model: function(params) {
-        return App.PRODUCTS.findBy('title', params.title);
+        return this.store.find('product', params.product_id);
     }
 });
 
-App.PRODUCTS = [
+App.Product = DS.Model.extend({
+    title: DS.attr('string'),
+    price: DS.attr('number'),
+    description: DS.attr('string'),
+    isOnSale: DS.attr('boolean'),
+    image: DS.attr('string')
+});
+
+App.Product.FIXTURES = [
     {
+        id: 1,
         title: 'Flint',
         price: 99,
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat, dui vitae sodales laoreet, eros enim accumsan neque, sit amet eleifend dolor urna ultricies diam. Mauris ac pellentesque odio, ut adipiscing massa. Integer eget tempor sem. Mauris sodales mi ultricies, ornare velit vel, mollis nibh. ',
@@ -46,6 +57,7 @@ App.PRODUCTS = [
         image: 'http://placehold.it/250'
     },
     {
+        id: 2,
         title: 'Kindling',
         price: 249,
         description: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed semper eros ipsum, in condimentum leo pulvinar id. Aliquam egestas ut mi non vestibulum. Phasellus laoreet at enim vitae porttitor. Mauris pulvinar auctor justo, nec pulvinar dui. Ut placerat pulvinar dolor, ac lobortis purus. Proin ornare eget odio ac facilisis.',
@@ -53,6 +65,7 @@ App.PRODUCTS = [
         image: 'http://placehold.it/250'
     },
     {
+        id: 3,
         title: 'Matches',
         price: 39,
         description: 'Suspendisse tincidunt justo eu diam tincidunt auctor. Nam ac odio lacus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque varius euismod auctor. Ut sollicitudin dui eu magna vestibulum hendrerit. In hac habitasse platea dictumst. Praesent sit amet consectetur ligula. ',
